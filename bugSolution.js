bd2aae814e088486553fff4b4bfb9fd55e1e3d71
BugHunter-X <askzenai@gmail.com>
2025-02-13 06:32:47 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,18 @@
+```javascript
+import Link from 'next/link';
+
+function MyComponent() {
+  return (
+    <div>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link> 
+    </div>
+  );
+}
+
+export default MyComponent;
+```
